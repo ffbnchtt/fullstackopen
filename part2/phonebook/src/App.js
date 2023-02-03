c5b08acfaac58c3f6559ecf18e7dbf13f226f69d
Federico Bianchetti <ffbnchtt@gmail.com>
2023-02-03 17:59:21 -0300

    innit phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>name: <input onChange={handleNewName} value={newName} /></div>
+        <div>number: <input onChange={handleNewNumber} value={newNumber} /></div>
+        <div><button type="submit">add</button></div>
+      </form>
+      <h3>Add a new</h3>
+
+      <h3>Numbers</h3>
+
+      {persons.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
