009f218459dee2556947ad9764acf8922e3921ac
Federico Bianchetti <ffbnchtt@gmail.com>
2023-02-03 17:23:03 -0300

    add courseinfo and phonebook folders

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,9 @@
+import Part from "./Part"
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => <Part key="" part={part.name} exercises={part.exercises} />)}
+        </div>
+    )
+}
+export default Content
