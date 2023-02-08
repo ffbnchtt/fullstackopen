04646c285ddfd4d5a2d2fcffc0e0c376a3d87596
Federico Bianchetti <ffbnchtt@gmail.com>
2023-02-08 17:56:42 -0300

    add db json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import { personsService } from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -10,37 +10,69 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => setPersons(response.data));
-  }, []);
+  const handleNewName = (event) => setNewName(event.target.value);
+  const handleNewNumber = (event) => setNewNumber(event.target.value);
+  const handleFilter = (event) => setFilter(event.target.value);
 
-  const validateName = (newName) => {
-    let result = persons.some((person) => person.name === newName);
-    !result && alert(`${newName} is already added to phonebook`);
-    return result;
-  };
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then((response) => setPersons(response))
+      .catch((error) => console.error(error));
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (!validateName(newName)) return;
-
-    const personObject = {
+    const newPerson = {
       name: newName,
       number: newNumber,
       id: persons.length + 1,
     };
 
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    if (!validatePerson(newPerson)) return;
+
+    personsService
+      .create(newPerson)
+      .then((response) => {
+        setPersons(persons.concat(response));
+        setNewName("");
+        setNewNumber("");
+      })
+      .catch((error) => console.error(error));
   };
 
-  const handleNewName = (event) => setNewName(event.target.value);
-  const handleNewNumber = (event) => setNewNumber(event.target.value);
-  const handleFilter = (event) => setFilter(event.target.value);
+  /*const updatePerson = () => {
+    personsService
+      .update()
+      .then(() => getPersons())
+      .catch((error) => console.error(error));
+  };*/
+
+  const deletePerson = (event) => {
+    if (
+      window.confirm(
+        `Are you sure you wish to delete ${event.target.dataset.name} (${event.target.dataset.number}) ?`
+      )
+    ) {
+      personsService
+        .remove(event.target.dataset.id)
+        .then(() =>
+          setPersons(
+            persons.filter(
+              (person) => person.id.toString() !== event.target.dataset.id
+            )
+          )
+        )
+        .catch((error) => console.error(error));
+    }
+  };
+
+  const validatePerson = (newName) => {
+    let result = persons.some((person) => person.name === newName);
+    !result && alert(`${newName} is already added to phonebook`);
+    return result;
+  };
 
   return (
     <div>
@@ -57,8 +89,7 @@ const App = () => {
       />
 
       <h3>Numbers</h3>
-
-      <Persons persons={persons} filter={filter} />
+      <Persons filter={filter} persons={persons} deletePerson={deletePerson} />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,11 +1,20 @@
-const Persons = ({ persons, filter }) => {
+const Persons = ({ filter, persons, deletePerson }) => {
     return (
         <>
             {persons
                 .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
                 .map(person => (
                     <div key={person.id}>
-                        {person.name} {person.number}
+                        <span>
+                            {person.name} {person.number}
+                        </span>
+                        <button
+                            data-id={person.id}
+                            data-name={person.name}
+                            data-number={person.number}
+                            onClick={deletePerson}>
+                            delete
+                        </button>
                     </div>
                 ))}
         </>
