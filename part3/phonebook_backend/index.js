3a92faed8d50bbe1175654fec54c04b29acad6ef
Federico Bianchetti <ffbnchtt@gmail.com>
2023-02-16 11:38:03 -0300

    add delete and update

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,57 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
 const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 app.use(morgan('tiny'))
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url =
-    `mongodb://phonebook:${password}@ac-jvttjzb-shard-00-00.cfo0olc.mongodb.net:27017,ac-jvttjzb-shard-00-01.cfo0olc.mongodb.net:27017,ac-jvttjzb-shard-00-02.cfo0olc.mongodb.net:27017/?ssl=true&replicaSet=atlas-8ewk2f-shard-0&authSource=admin&retryWrites=true&w=majority`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    number: String
-})
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 app.get("/", (request, response) => {
     response.status(404).end()
@@ -73,9 +29,8 @@ app.get("/api/persons", (request, response) => {
             response.json(persons);
         })
 });
-//mongoose.connection.close()
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             if (person) {
@@ -84,6 +39,7 @@ app.get('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             }
         })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -113,11 +69,26 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
-    response.status(204).end()
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => response.status(204).end())
+        .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -126,6 +97,24 @@ const generateId = () => {
         : 0
     return maxId + 1
 }
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
