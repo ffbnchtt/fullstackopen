7b1351e990748ac1e4d47183f7a4f78713ee9c67
Federico Bianchetti <ffbnchtt@gmail.com>
2023-02-16 10:57:13 -0300

    fix Person instance

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -101,11 +101,11 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
         id: generateId(),
-    }
+    })
 
     //persons = persons.concat(person)
     person.save().then(savedPerson => {
